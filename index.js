c0948cc112058e41d51cdf3cddbd378d59a4a5aa
gaspare bastone <gaspareb@yahoo.com>
2024-01-30 17:13:42 -0500

    updates

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const path = require('path')
 
 const PORT = process.env.PORT || 5001
 
-express()
+const app = express()
   .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+  app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })

977b3a38d5e08c8478bac11db1fd16d19bc1046c
gaspare bastone <gaspareb@yahoo.com>
2024-01-31 12:40:31 -0500

    path fix

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/test', (req, res) => {
   res.send('Hello World!')
 })
 
+app.post('/posttest', (req, res) => {
+  res.send('Hello Worl3!')
+})
+
 app.listen(PORT, () => {
   console.log(`TDXAPI App listening on port ${PORT}`)
 })

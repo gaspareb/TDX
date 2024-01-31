509beb934a1c3ecad0ecb4fbc4beeae085f008b6
gaspare bastone <gaspareb@yahoo.com>
2024-01-31 11:45:30 -0500

    path fix

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 const express = require('express')
-const path = require('path')
 
 const PORT = process.env.PORT || 5001
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-const app = express()
-
 app.get('/test', (req, res) => {
-  res.send('Hello World2!')
+  res.send('Hello World!')
 })
 
-app.get('/', (req, res) => {
-  res.send('Hello World1!')
+app.listen(port, () => {
+  console.log(`Example app listening on port ${PORT}`)
 })

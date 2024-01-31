95d9c4bf52318a07f6c9d220a10e0293dfdf07af
gaspare bastone <gaspareb@yahoo.com>
2024-01-31 15:06:07 -0500

    path fix

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5001
 
+app.get('/', (req, res) => {
+  res.send('Hello World OG!')
+})
+
 app.get('/test', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World 1!')
+})
+
+app.post('/posttest2', (req, res) => {
+  res.send('Hello World 3!')
 })
 
 app.post('/posttest', (req, res) => {
-  res.send('Hello Worl3!')
+  res.send('Hello World 3!')
 })
 
 app.listen(PORT, () => {

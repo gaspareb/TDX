219f4b1daf70cca8b38bb85af40b050e6f9cf216
gaspare bastone <gaspareb@yahoo.com>
2024-06-07 16:03:28 -0400

    path fix

--- index.js
+++ index.js
@@ -29,3 +29,21 @@ app.get('/clickSFEvent', (req, res) => {
 app.listen(PORT, () => {
   console.log(`TDXAPI App listening on port ${PORT}`)
 })
+
+function verifySignature(req, res, buf, encoding) {
+  const signature = req.header('x-adsk-signature');
+  if(!signature) { return; }
+
+  // use utf-8 encoding by default
+  const body    = buf.toString(encoding);
+  const hmac    = crypto.createHmac('sha1', WEBHOOKS_SECRET);
+  const calcSignature = 'sha1hash=' + hmac.update(body).digest('hex');
+  req.signature_match = (calcSignature === signature);
+}
+
+app.use(bodyParser.json({
+  inflate: true,
+  limit: '1024kb',
+  type: 'application/json',
+  verify: verifySignature
+}));

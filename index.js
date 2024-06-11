1cd5ef99bc57cdfc7a881ec07d093a00bc19c6ab
gaspare bastone <gaspareb@yahoo.com>
2024-06-11 10:57:31 -0400

    path fix

--- index.js
+++ index.js
@@ -21,10 +21,13 @@ function verifyPostData(req, res, next) {
   if (!req.rawBody) {
     return next('Request body empty')
   }
-
+  console.log('req.rawBody=>' + req.rawBody);
   const sig = Buffer.from(req.get(sigHeaderName) || '', 'utf8')
+  console.log('sig=>' + sig);
   const hmac = crypto.createHmac(sigHashAlg, secret)
+  console.log('hmac=>' + hmac);
   const digest = Buffer.from(sigHashAlg + '=' + hmac.update(req.rawBody).digest('hex'), 'utf8')
+  console.log('digest=>' + digest);
   if (sig.length !== digest.length || !crypto.timingSafeEqual(digest, sig)) {
     return next(`Request body digest (${digest}) did not match ${sigHeaderName} (${sig})`)
   }

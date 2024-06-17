3e2a684970d25f94a602a56897cf7fc6173db612
gaspare bastone <gaspareb@yahoo.com>
2024-06-17 10:29:59 -0400

    path fix

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ function verifyPostData(req, res, next) {
  
     //const digest = Buffer.from(sigHashAlg + '=' + hmac.update(req.rawBody).digest('base64'), 'utf8');
     //const digest = Buffer.from(sigHashAlg + '=' + hmac.update(req.rawBody).digest('hex'), 'utf8');
+    hmac.update(req.rawBody);
     const digest = hmac.digest('base64');  
     console.log('digest: ' + digest);
 
